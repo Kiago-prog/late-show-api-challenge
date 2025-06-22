05d440cf6caa78b1235feb24f1685b268cb33fd5
kiago <roy.kiago@student.moringaschool.com>
2025-06-22 20:23:40 +0300

    DEBUG

--- server/models/episode.py
+++ server/models/episode.py
@@ -1,10 +1,13 @@
-from server.app import db
+from . import db
 
 class Episode(db.Model):
     __tablename__ = 'episodes'
-
+    
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date, nullable=False)
-    number = db.Column(db.Integer, nullable=False)
-
-    appearances = db.relationship("Appearance", back_populates="episode", cascade="all, delete-orphan")
+    title = db.Column(db.String(100))
+    air_date = db.Column(db.Date)
+    
+    show_id = db.Column(db.Integer, db.ForeignKey('shows.id'), nullable=False)
+    show = db.relationship('Show', back_populates='episodes')
+    
+    guests = db.relationship('Guest', back_populates='episode', cascade='all, delete-orphan')
