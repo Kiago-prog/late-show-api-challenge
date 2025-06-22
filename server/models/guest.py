83d7c7576cf4bfa2ebb3c33b2c117a6f25b5545e
kiago <roy.kiago@student.moringaschool.com>
2025-06-22 20:24:00 +0300

    DEBUG

--- server/models/guest.py
+++ server/models/guest.py
@@ -1,10 +1,10 @@
-from server.app import db
+from . import db
 
 class Guest(db.Model):
     __tablename__ = 'guests'
-
+    
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    occupation = db.Column(db.String)
-
-    appearances = db.relationship("Appearance", back_populates="guest", cascade="all, delete-orphan")
+    name = db.Column(db.String(100), nullable=False)
+    
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
+    episode = db.relationship('Episode', back_populates='guests')
