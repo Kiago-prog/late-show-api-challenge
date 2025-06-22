431857d48af9604c0be227c67f3a7438d0a6f0f7
kiago <roy.kiago@student.moringaschool.com>
2025-06-22 10:54:20 +0300

    Guest

---
+++ server/models/appearance.py
@@ -0,0 +1,21 @@
+from server.app import db
+
+class Appearance(db.Model):
+    __tablename__ = 'appearances'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
+
+    guest = db.relationship("Guest", back_populates="appearances")
+    episode = db.relationship("Episode", back_populates="appearances")
+
+    # Simple validation in the model (can also be done via Marshmallow or controller)
+    def __init__(self, rating, guest_id, episode_id):
+        if rating < 1 or rating > 5:
+            raise ValueError("Rating must be between 1 and 5")
+        self.rating = rating
+        self.guest_id = guest_id
+        self.episode_id = episode_id
