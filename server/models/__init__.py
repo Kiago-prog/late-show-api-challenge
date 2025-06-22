2e550cfdb70b59df06b89531d72236a35c6ec4ef
kiago <roy.kiago@student.moringaschool.com>
2025-06-22 21:11:32 +0300

    DEBUG

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -6,3 +6,6 @@ from .user import User
 from .show import Show
 from .episode import Episode
 from .guest import Guest
+from .appearance import Appearance
+
+__all__ = ["db", "User", "Show", "Episode", "Guest", "Appearance"]
