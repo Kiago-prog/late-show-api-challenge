5f0a734678198655850397f3415f8539904a7339
kiago <roy.kiago@student.moringaschool.com>
2025-06-22 21:18:15 +0300

    DEBUG

--- server/models/guest.py
+++ server/models/guest.py
@@ -5,6 +5,9 @@ class Guest(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    occupation = db.Column(db.String(100))  # Optional but common
     
-    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
-    episode = db.relationship('Episode', back_populates='guests')
+    appearances = db.relationship('Appearance', back_populates='guest', cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f"<Guest {self.name}>"
